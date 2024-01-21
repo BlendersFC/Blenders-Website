2f4cbf9848f5b65ddc84def2857a4ca960b1315a
Iker Casillas <ikercasillasp2@gmail.com>
2024-01-21 14:08:11 -0600

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `BlendBots FC`,
+    description: `Humanoid soccer team.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
