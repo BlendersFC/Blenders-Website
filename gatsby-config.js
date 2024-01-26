016bb095715ac29c4e182ecd25b23420ed4617c5
Iker Casillas <ikercasillasp2@gmail.com>
2024-01-26 17:37:56 -0600

    change icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/robotec-icon.ico`, // This path is relative to the root of the site.
+        icon: `src/images/robotec-png-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
